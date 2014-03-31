328c30018482d62b1c15d18fb0c797551e8994c7
nenad <nisavicnenad@gmail.com>
2014-04-01 01:05:47 +0200

    add script file - separate functions

---
+++ myFunctions.js
@@ -0,0 +1,45 @@
+var answers = ['knock off', 'tighten', 'splash', 'rip off', 'set back', 'pick up'];
+		
+		function showAnswers(){
+			var answers = document.getElementsByClassName('answer');
+			for(var i=0; i < answers.length; i++){
+				answers[i].style.visibility = 'visible';
+			}
+			hightLightAnwers();
+			document.getElementById('check').style.display = 'none';
+			document.getElementById('again').style.visibility = 'visible';
+		};
+		
+		function checkAnswer(yourAnswer, rightAnswer){
+			return yourAnswer == rightAnswer;
+		}
+		
+		function getAnswer(index){
+			var selectTag = document.getElementById('answer'+index);
+			return selectTag.options[selectTag.selectedIndex].text;
+		}
+		
+		function hightLightAnwers(){
+		
+			for(var i = 0; i < answers.length; i++){
+				if(getAnswer(i+1) == answers[i]){
+					hightlightRight(i+1);
+				}else{
+					hightlightWrong(i+1);
+				}
+			}
+		}
+		
+		
+		
+		function hightlightWrong(index){
+			var selectTag = document.getElementById('answer'+index);
+				selectTag.style.textDecoration = "line-through";
+				selectTag.style.color = 'red';
+				selectTag.disabled = true;
+		}
+		
+		function hightlightRight(index){
+			var selectTag = document.getElementById('answer'+index);
+			selectTag.style.color = 'green';
+		}
